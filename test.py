4e18b26f332d184e98f737809f8544759e13cbbd
Fuuya1102668 <101689724+Fuuya1102668@users.noreply.github.com>
2022-08-26 17:38:27 +0900

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import speech_recognition as sr
-
-r = sr.Recognizer()
-with sr.AudioFile("test.wav") as source:
-    audio = r.record(source)
-text = r.recognize_google(audio, language="ja-JP")
-print(text)
-
